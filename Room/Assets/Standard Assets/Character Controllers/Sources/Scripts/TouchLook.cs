1b4b53bd4dfc3e679961b52a97306ece0d8fe005
Peichen <pcchang77@gmail.com>
2014-02-22 15:13:28 -0800

    add new view port

--- Room/Assets/Standard Assets/Character Controllers/Sources/Scripts/TouchLook.cs
+++ Room/Assets/Standard Assets/Character Controllers/Sources/Scripts/TouchLook.cs
@@ -18,47 +18,74 @@ using System.Collections;
 public class TouchLook : MonoBehaviour
 {
 
-		enum TouchLookCommand
-		{
-				TlCmd_TurnLeft=0,
-				TlCmd_TurnRight,
-				TlCmd_None
+	enum TouchLookCommand
+	{
+		TlCmd_TurnLeft=0,
+		TlCmd_TurnRight,
+		TlCmd_None
+	}
+	enum TouchLookView
+	{
+		TlVw_Normal=0,
+		TlVw_Top
+	}
+	public float sensitivityX = 1F;
+	public float minimumX = -360F;
+	public float maximumX = 360F;
+	public float guiboxHeight = 30F;
+	public float guiboxWidth = 200F;
+	float posLeft = 5F;
+	float posTop = 5F;
+	TouchLookCommand touchLookCmd = TouchLookCommand.TlCmd_None;
+	TouchLookView touchLookView = TouchLookView.TlVw_Normal;
+	bool changeView = false;
+	float vwCapNormalPosY = 20.0F;
+	float vwCapTopPosY = 80.0F;
+
+
+	void Update ()
+	{
+			//if (!Input.GetMouseButton (0))
+			//	return;
+			//transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+
+		if (touchLookCmd != TouchLookCommand.TlCmd_None && !changeView) {
+			switch (touchLookCmd) {
+			case TouchLookCommand.TlCmd_TurnLeft:
+			// Turn Left
+				transform.Rotate (0, -sensitivityX, 0);
+				break;
+			case TouchLookCommand.TlCmd_TurnRight:
+			// Turn Right
+				transform.Rotate (0, sensitivityX, 0);
+				break;
+			}
+			touchLookCmd = TouchLookCommand.TlCmd_None;
 		}
-		public float sensitivityX = 1F;
-		public float minimumX = -360F;
-		public float maximumX = 360F;
-		public float guiboxHeight = 30F;
-		public float guiboxWidth = 200F;
-		float posLeft = 5F;
-		float posTop = 5F;
-		TouchLookCommand touchLookCmd = TouchLookCommand.TlCmd_None;
-
-		void Update ()
+		if (changeView) 
 		{
-				//if (!Input.GetMouseButton (0))
-				//	return;
-				//transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
-
-				if (touchLookCmd != TouchLookCommand.TlCmd_None) {
-						switch (touchLookCmd) {
-						case TouchLookCommand.TlCmd_TurnLeft:
-						// Turn Left
-								transform.Rotate (0, -sensitivityX, 0);
-								break;
-						case TouchLookCommand.TlCmd_TurnRight:
-						// Turn Right
-								transform.Rotate (0, sensitivityX, 0);
-								break;
-						}
-						touchLookCmd = TouchLookCommand.TlCmd_None;
+				Vector3 pos = transform.localPosition;
+				switch(touchLookView)
+				{
+				case TouchLookView.TlVw_Normal:
+					pos.y = vwCapNormalPosY;
+					transform.Rotate(-90,0,0);
+					break;
+				case TouchLookView.TlVw_Top:
+					pos.y = vwCapTopPosY;
+					transform.Rotate(90,0,0);
+					break;
 				}
+				transform.localPosition = pos;
+				changeView = false;
 		}
+	}
 
-		void Start ()
-		{
-				// Make the rigid body not change rotation
-				if (rigidbody)
-						rigidbody.freezeRotation = true;
+	void Start ()
+	{
+			// Make the rigid body not change rotation
+		if (rigidbody)
+				rigidbody.freezeRotation = true;
 //				GameObject screen = GameObject.Find("Screen");
 //				if (screen) 
 //				{
@@ -66,17 +93,26 @@ public class TouchLook : MonoBehaviour
 //						scale.y = scale.y * (float)(0.5);
 //						screen.transform.localScale = scale;
 //				}
-		}
+	}
 
-		void OnGUI ()
-		{
-				if (GUI.RepeatButton (new Rect (posLeft, posTop, guiboxWidth, guiboxHeight), "Left")) {
-						touchLookCmd = TouchLookCommand.TlCmd_TurnLeft;
-				}
-				if (GUI.RepeatButton (new Rect (Screen.width - posLeft - guiboxWidth, posTop, guiboxWidth, guiboxHeight), "Right")) {
-						touchLookCmd = TouchLookCommand.TlCmd_TurnRight;
-				}
-				//GUI.Label (new Rect (posLeft, posTop + guiboxHeight , guiboxWidth, guiboxHeight), touchX.ToString ());
-				//GUI.Label (new Rect (posLeft, posTop + guiboxHeight * 2, guiboxWidth, guiboxHeight), touchY.ToString ());
+	void OnGUI ()
+	{
+		if (touchLookView == TouchLookView.TlVw_Normal) {
+			if (GUI.RepeatButton (new Rect (posLeft, posTop, guiboxWidth, guiboxHeight), "Left")) {
+				touchLookCmd = TouchLookCommand.TlCmd_TurnLeft;
+			}
+			if (GUI.RepeatButton (new Rect (Screen.width - posLeft - guiboxWidth, posTop, guiboxWidth, guiboxHeight), "Right")) {
+				touchLookCmd = TouchLookCommand.TlCmd_TurnRight;
+			}
+			if (GUI.Button (new Rect (Screen.width/2 - guiboxWidth/2, posTop, guiboxWidth, guiboxHeight), "Top View")) {
+				touchLookView = TouchLookView.TlVw_Top;
+				changeView = true;
+			}
+		} else if (touchLookView == TouchLookView.TlVw_Top) {
+			if (GUI.Button (new Rect (Screen.width/2 - guiboxWidth/2, posTop, guiboxWidth, guiboxHeight), "Normal View")) {
+				touchLookView = TouchLookView.TlVw_Normal;
+				changeView = true;
+			}
 		}
+	}
 }
